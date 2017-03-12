1358628c5d8b9415837a9a2a41f3948ea89a6002
DimVisioN <pandemic91@gmail.com>
2017-03-12 14:14:15 +0200

    added homework

---
+++ SofUniSelenium/src/test/java/SoftUniTest.java
@@ -0,0 +1,102 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by ggeor on 11-Mar-17.
+ */
+public class SoftUniTest {
+    private WebDriver driver;
+    private static final String LOGIN_USERNAME = "bqwe";
+    private static final String LOGIN_PASSWORD = "9605046960";
+    // before
+    @Before
+    public void setUp() {
+        // code executed before each test
+        System.setProperty("webdriver.chrome.driver", "E:\\QA STUFF\\chromedriver.exe");
+        this.driver = new ChromeDriver();
+        this.driver
+                .manage()
+                .timeouts()
+                .implicitlyWait(10, TimeUnit.SECONDS);
+        this.driver
+                .manage()
+                .window()
+                .maximize();
+    }
+
+    private void login() {
+        this.driver.get("https://www.abv.bg/");
+
+        WebElement usernameField
+                = this.driver
+                .findElement(By.id("username"));
+        usernameField.sendKeys(LOGIN_USERNAME);
+
+        WebElement passwordField
+                = this.driver
+                .findElement(By.id("password"));
+        passwordField.sendKeys(LOGIN_PASSWORD);
+
+        WebElement loginButton
+                =this.driver
+                .findElement(By.id("loginBut"));
+        loginButton.click();
+
+    }
+
+    // test method          whatToTest_whatConditions_expectedResult
+    @Test
+    public void testLogin_validCredentials_expectedNavigationToProfile(){
+        this.login();
+        Assert.assertEquals("The profile URL should be: " + "https://nm60.abv.bg/Mail.html",
+                "https://nm60.abv.bg/Mail.html",
+                this.driver.getCurrentUrl()
+        );
+    }
+
+    @Test
+    public void testLogin_validCredentials_expectedUserName(){
+        this.login();
+
+        WebElement loggedUserNameField
+                =this.driver
+                .findElement(By.xpath("//*[@id=\"middlePagePanel\"]/div[1]/div[1]"));
+        String loggedUserNameText = loggedUserNameField.getText().trim();
+        boolean expectedUsername = loggedUserNameText.endsWith("Georgi Georgiev");
+        Assert.assertTrue("User Name should end with" + "Georgi Georgiev", expectedUsername);
+    }
+
+    @Test
+    public void testLogin_validCredentials_expectedUserEmail(){
+        this.login();
+
+        WebElement loggedUserEmail
+                =this.driver
+                .findElement(By.xpath("//*[@id=\"middlePagePanel\"]/div[1]/div[2]"));
+
+        String expectedEmail = "bqwe@abv.bg";
+        Assert.assertEquals("Email should be: " + "(" + expectedEmail + ")",
+                "(" + expectedEmail + ")", loggedUserEmail
+                        .getText()
+                        .trim()
+        );
+    }
+
+    // after
+    @After
+    public void tearDown(){
+        //what happens after each test
+        this.driver.quit();
+    }
+}
