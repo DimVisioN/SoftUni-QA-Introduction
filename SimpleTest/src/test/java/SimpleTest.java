90ac5f4533fbf84790dd82355efbd99a1a0089d3
DimVisioN <pandemic91@gmail.com>
2017-03-13 16:32:05 +0200

    added a simple test

---
+++ SimpleTest/src/test/java/SimpleTest.java
@@ -0,0 +1,22 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by ggeor on 13-Mar-17.
+ */
+public class SimpleTest {
+    @Before
+    public void setUp(){
+
+    }
+    @Test
+    public void whatToTest_whatConditions_expectedResult(){
+        Assert.fail();
+    }
+    @After
+    public void tearDown(){
+
+    }
+}
